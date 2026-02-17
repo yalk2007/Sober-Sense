c377e03120e2844a5ab5408e04168a8928ee4c11
Lucy Shah <shah.lu@northeastern.edu>
2026-02-16 20:14:23 -0500

    added doccumentation to constants

--- sober-sense/sober-sense/Constants.swift
+++ sober-sense/sober-sense/Constants.swift
@@ -8,42 +8,46 @@ import Foundation
 import SwiftUI
 
 struct Constants {
+    
+    // Navigation Strings
     static let homeString: String = "Home"
     static let profileString: String = "Profile"
     static let dataString: String = "Data"
     static let startString = "start"
     
-    
+    // Symbol Icons
     static let homeIconString = "house"
     static let profileIconString = "person"
     static let dataIconString = "chart.bar"
     
-    
+    // URLs
     static let testTitleUrl = "https://image2url.com/r2/default/images/1770165050991-ace753c2-f93f-4df7-af5a-dff2d08894ff.jpg"
 }
 
-
-extension Text{
+// Text formats/style conventions
+extension Text {
+    
     func ghostButton() -> some View {
         self
             .frame(width: 100, height: 50)
             .foregroundStyle(.buttonText)
             .bold()
-            .background{
+            .background {
                 RoundedRectangle(cornerRadius: 20, style: .continuous)
-                    .stroke(.buttonBorder,lineWidth: 5)
-
+                    .stroke(.buttonBorder, lineWidth: 5)
             }
     }
-    func textUniversal() -> some View{
+    
+    func textUniversal() -> some View {
         self
             .foregroundStyle(.text)
             .font(.system(size: 22))
             .bold()
     }
-    func numberText() -> some View{
+    
+    func numberText() -> some View {
         self
             .font(.system(size: 100))
             .bold()
-        }
+    }
 }
